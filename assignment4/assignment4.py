2411f9e7bd4596b1d4be80e458b8749febb78ac6
Shubham123ssr <100044183+Shubham123ssr@users.noreply.github.com>
2022-09-01 07:50:34 +0530

    Add files via upload

---
+++ assignment4/assignment4.py
@@ -0,0 +1,57 @@
+
+'''
+#q1
+name=input("enter your name:")
+print(name)
+
+#q2
+
+a=input("enter any number:")
+print(a)
+
+#q3
+a=input("enter the value of a:")
+b=input("enter the value of b :")
+print(int(a)+int(b))
+
+#q4
+r=input("enter the value of radius:")
+a=3.14*float(r)*float(r)
+print("Area of circle"+"=",a)
+
+#q5
+a=input("enter the value of a:")
+print(int(a)*int(a))
+
+#q6
+h=input("enter the value of height:")
+b=input("enter the value of base :")
+a=1/2*float(b)*float(h)
+print(a)
+
+#q7
+a=input("enter the value of a:")
+b=input("enter the value of b :")
+c=input("enter the value of c :")
+print((int(a)+int(b)+int(c))/3)
+
+#q8
+s=input("enter the value of s:")
+r=input("enter the value of r :")
+t=input("enter the value of t :")
+print((int(s)*float(r)*int(t))/100)
+
+#q9
+a=input("enter the value of a:")
+b=input("enter the value of b :")
+c=input("enter the value of c :")
+print(int(a)*int(b)*int(c))
+
+#q10
+a=input("enter the value of a:")
+b=input("enter the value of b :")
+
+print(int(a)*int(b))
+'''
+
+
